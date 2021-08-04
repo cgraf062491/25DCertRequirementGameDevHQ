5d0318e391c57c182f76bfb016c5505dbf622738
cgraf062491 <cgraf062491@gmail.com>
2021-08-04 13:22:31 -0400

    Christopher Graf - Moving Platform - 08/04/21
    Completed the Moving Platform in the Phase 1: Framework of the 2.5D Game Course

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MovingPlatform.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetA, _targetB;
+    [SerializeField]
+    private float _speed = 3.0f;
+    private bool _switching = false;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_switching == false)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, _targetB.position, _speed * Time.deltaTime);
+        }
+        else if (_switching == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetA.position, _speed * Time.deltaTime);
+        }
+
+        if (transform.position == _targetB.position)
+        {
+            _switching = true;
+        }
+        else if (transform.position == _targetA.position)
+        {
+            _switching = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
